474039716c454be16c77a5782d34d2eec291545a
Ryu <87907656+Ryu0118@users.noreply.github.com>
2023-06-08 12:49:22 +0900

    add recursivelyAllKeyPaths

--- Sources/KeyPathIterable/KeyPathIterable.swift
+++ Sources/KeyPathIterable/KeyPathIterable.swift
@@ -1,3 +1,43 @@
 public protocol KeyPathIterable {
     static var allKeyPaths: [PartialKeyPath<Self>] { get }
+    static var allAnyKeyPaths: [AnyKeyPath] { get }
+
+    var allKeyPaths: [PartialKeyPath<Self>] { get }
+    var allAnyKeyPaths: [AnyKeyPath] { get }
+
+    var recursivelyAllKeyPaths: [PartialKeyPath<Self>] { get }
+    var recursivelyAllAnyKeyPaths: [AnyKeyPath] { get }
+}
+
+public extension KeyPathIterable {
+    static var allAnyKeyPaths: [AnyKeyPath] {
+        allKeyPaths.map { $0 as AnyKeyPath }
+    }
+
+    var allKeyPaths: [PartialKeyPath<Self>] {
+        Self.allKeyPaths
+    }
+
+    var allAnyKeyPaths: [AnyKeyPath] {
+        allKeyPaths.map { $0 as AnyKeyPath }
+    }
+
+    var recursivelyAllKeyPaths: [PartialKeyPath<Self>] {
+        var recursivelyKeyPaths = [PartialKeyPath<Self>]()
+        for keyPath in allKeyPaths {
+            recursivelyKeyPaths.append(keyPath)
+            if let anyKeyPathIterable = self[keyPath: keyPath] as? any KeyPathIterable {
+                for childKeyPath in anyKeyPathIterable.recursivelyAllAnyKeyPaths {
+                    if let appendedKeyPath = keyPath.appending(path: childKeyPath) {
+                        recursivelyKeyPaths.append(appendedKeyPath)
+                    }
+                }
+            }
+        }
+        return recursivelyKeyPaths
+    }
+
+    var recursivelyAllAnyKeyPaths: [AnyKeyPath] {
+        recursivelyAllKeyPaths.map { $0 as AnyKeyPath }
+    }
 }
